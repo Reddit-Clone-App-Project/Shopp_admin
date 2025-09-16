5dd2a6dc508b00b0c1bfc8860219808dfe134b9e
NGHIEM GIA BAO <99817858+baoOyster@users.noreply.github.com>
2025-09-16 23:46:30 +0700

    Configure/testing (#5)
    * Fix directing after login bugand install package for testing
    
    * configuring vitest
    
    * Configuration for mocking

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,20 +1,6 @@
-import React, { useEffect } from 'react'
 import Logo from '../assets/logo.svg';
 import LoginForm from '../features/LoginUser/LoginForm';
-import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import type { RootState } from '../redux/store';
-
 const LoginPage = () => {
-  const { isLoggedIn } = useSelector((state: RootState) => state.loginReducer);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/home');
-    }
-  }, [isLoggedIn, navigate]);
-
   return (
     <div className='bg-black w-full h-screen flex flex-col items-center justify-center text-white space-y-6'>
       <img src={Logo} alt="Logo" />
